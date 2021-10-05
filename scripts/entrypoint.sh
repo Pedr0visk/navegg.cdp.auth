fd5e7f35d0fba4671087f194072df57c80467193
Pedro Henrique <pedro357bm@gmail.com>
2021-10-05 10:32:33 -0300

    collect static path removed from entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,5 +6,5 @@ echo "finished migration"
 
 echo "Running server..."
 echo ${USER}
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 gunicorn --bind 0.0.0.0:9000 core.wsgi
