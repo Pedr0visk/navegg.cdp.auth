c6179a0377e10b29791f9dc73ec785c3f4074595
Pedro Henrique <pedro357bm@gmail.com>
2021-11-09 13:18:26 -0300

    fixed users url

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import include
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/users/', include('users.urls'))
 ]
